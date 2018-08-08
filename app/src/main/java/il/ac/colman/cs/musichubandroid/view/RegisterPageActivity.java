ecbf0ed8a05429c93c299c5985fbdad68bc4deda
Aviv Ostrovsky <ostrovskyaviv@gmail.com>
2018-08-08 21:10:47 +0300

    Registration page working (need to add a connection with the controller)

--- app/src/main/java/il/ac/colman/cs/musichubandroid/view/RegisterPageActivity.java
+++ app/src/main/java/il/ac/colman/cs/musichubandroid/view/RegisterPageActivity.java
@@ -1,42 +1,39 @@
 package il.ac.colman.cs.musichubandroid.view;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.provider.ContactsContract;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.util.Patterns;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import il.ac.colman.cs.musichubandroid.R;
+import il.ac.colman.cs.musichubandroid.datatypes.Artist;
+import il.ac.colman.cs.musichubandroid.model.ArtistsTable;
 
-import static android.text.InputType.TYPE_TEXT_VARIATION_PASSWORD;
-import static il.ac.colman.cs.musichubandroid.R.id.Email;
 
-/**
- * Created by ostrovskyav on 08/08/2018.
- */
 
 public class RegisterPageActivity extends AppCompatActivity {
 
-
+    Button registerButton;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_page);
         setTitle("Register page");
+        registerButton = (Button)findViewById(R.id.registerButton);
+        HandlerRegister();
     }
 
-    public boolean isPasswordValid()
+    private boolean isPasswordValid()
     {
         EditText pass=(EditText)findViewById(R.id.password);
         EditText confirmPass=(EditText)findViewById(R.id.confirmPassword);
 
-       if(pass.getText().toString().equals(confirmPass.getText().toString()))
+       if(pass.getText().toString().equals(confirmPass.getText().toString())&&pass.getText().toString().length()>=8)
        {
            return true;
        }
@@ -44,9 +41,9 @@ public class RegisterPageActivity extends AppCompatActivity {
         return  false;
     }
 
-    public Boolean isEmailValid()
+    private Boolean isEmailValid()
     {
-        EditText emailField = (EditText) findViewById(R.id.Email);
+        EditText emailField = (EditText) findViewById(R.id.email);
         TextView emailErrorMessage=(TextView)findViewById(R.id.emailError);
         if(!TextUtils.isEmpty(emailField.getText()) && Patterns.EMAIL_ADDRESS.matcher(emailField.getText()).matches())
         {
@@ -59,12 +56,22 @@ public class RegisterPageActivity extends AppCompatActivity {
     }
 
 
-    public void register(View view)
+    public void HandlerRegister()
     {
-        if (isEmailValid()&&isPasswordValid())
-        {
-            Toast.makeText(this, "Registered",Toast.LENGTH_LONG).show();
-        }
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isEmailValid() && isPasswordValid()) {
+                    EditText userName = (EditText) findViewById(R.id.userName);
+                    EditText Password = (EditText) findViewById(R.id.password);
+                    EditText Email = (EditText) findViewById(R.id.email);
+                    Artist artist = new Artist(null, userName.getText().toString(), "", Email.getText().toString(), Password.getText().toString());
+                    ArtistsTable adder = new ArtistsTable();
+                    adder.addArtist(artist);
+                    finish();
+                }
+            }});
 
 
 
